8fed42c57a183fb784116a1bf5e5d855213044e4
Alikhan Serik <serikalikhan870@gmail.com>
2026-02-08 21:02:37 +0500

    init: initial backend

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+const { errorHandler } = require('./middlewares/errorHandler');
+const authRoutes = require('./routes/authRoutes');
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+app.use('/api/auth', authRoutes);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
